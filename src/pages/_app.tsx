88260d6748d85740c3a691a6102cf65c47376063
Dom0904 <johndominicang92@gmail.com>
2025-05-27 12:47:48 +0800

    fix: wrap app with NotificationProvider in _app.tsx to resolve useNotifications SSR/SSG error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { AuthProvider } from '../contexts/AuthContext';
 import { InventoryProvider } from '../contexts/InventoryContext';
 import { BOMProvider } from '../contexts/BOMContext';
+import { NotificationProvider } from '../contexts/NotificationContext';
 // Add other providers as needed
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -9,8 +10,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <AuthProvider>
       <InventoryProvider>
         <BOMProvider>
-          {/* Add more providers here if needed */}
-          <Component {...pageProps} />
+          <NotificationProvider>
+            {/* Add more providers here if needed */}
+            <Component {...pageProps} />
+          </NotificationProvider>
         </BOMProvider>
       </InventoryProvider>
     </AuthProvider>
